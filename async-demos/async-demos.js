d81e27e000833aeec999ae7dac0feca0983ddfa6
Magesh Kuppan <tkmagesh77@gmail.com>
2021-01-22 09:33:50 +0530

    async - assignment

---
+++ async-demos/async-demos.js
@@ -0,0 +1,39 @@
+console.log('async demos loaded');
+
+(function(){
+    //sync
+    function addSync(x,y){
+        console.log(`   @[service] processing ${x} and ${y}`);
+        const result = x + y;
+        console.log(`   @[service] returning result`);
+        return result;
+    }
+
+    function addSyncClient(x,y){
+        console.log(`@[client] triggering the sync operation`);
+        const result = addSync(x,y);
+        console.log(`@[client] result = ${result}`);
+    }
+
+    window['addSyncClient'] = addSyncClient;
+
+    //Async
+    function addAsync(x,y){
+        console.log(`   @[service] processing ${x} and ${y}`);
+        setTimeout(function(){
+            const result = x + y;
+            console.log(`   @[service] returning result`);
+            return result;
+        }, 4000);
+    }
+
+    function addAsyncClient(x,y){
+        console.log(`@[client] triggering the Async operation`);
+        const result = addAsync(x,y);
+        console.log(`@[client] result = ${result}`);
+    }
+
+    window['addAsyncClient'] = addAsyncClient;
+
+
+})();
